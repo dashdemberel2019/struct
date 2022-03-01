2c9d35af6fc28b39cc6498cc6510fd88c9104d4b
dashdemberel2019 <dashdmbrl@gmail.com>
2022-03-01 11:12:33 +0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace labConsole
+{
+    class Program
+    {
+        public struct Person
+        {
+            string name;
+            int age;
+            string gender;
+            string phone;
+            public void input()
+            {
+                name = Console.ReadLine();
+                age = Convert.ToInt32(Console.ReadLine());
+                gender = Console.ReadLine();
+                phone = Console.ReadLine();
+            }
+            public void print()
+            {
+                Console.WriteLine("{0,15:C} {1,5:N0} {2,10:C} {3,12:C}", name, age, gender, phone);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Person[] p = new Person[1];
+            for (int i = 0; i < p.Length; i++)
+                p[i].input();
+            for (int i = 0; i < p.Length; i++)
+                p[i].print();
+            Console.Read();
+        }
+    }
+}
